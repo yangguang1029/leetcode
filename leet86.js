50e1253d709a27cf833ccdfc1ec96926b61f829f
杨光1029 <yangguang1029@163.com>
2025-01-07 22:50:19 +0800

    1

--- leet86.js
+++ leet86.js
@@ -2,7 +2,7 @@
  * Definition for singly-linked list.
  * function ListNode(val, next) {
  *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
+ *     this.next = (next===undefined ? null : next) 
  * }
  */
 /**
